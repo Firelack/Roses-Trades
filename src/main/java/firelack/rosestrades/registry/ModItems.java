eb1ead9b6b69d83113d9663f24f6e61ea9d9c128
Firelack <firelack2000@gmail.com>
2025-09-04 17:24:26 +0200

    Add items in inventory
    - Add items in inventory
    - Change Specialrose item texture

--- src/main/java/firelack/rosestrades/registry/ModItems.java
+++ src/main/java/firelack/rosestrades/registry/ModItems.java
@@ -1,31 +1,39 @@
 package firelack.rosestrades.registry;
 
 import firelack.rosestrades.RosesTrades;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
-        public class ModItems {
-        public static final Item SPECIAL_ROSE = Registry.register(
-                Registries.ITEM,
-                Identifier.of(RosesTrades.MOD_ID, "special_rose"),
-                new SpecialRoseItem(new Item.Settings().maxCount(64))
-        );
+public class ModItems {
+    public static final Item SPECIAL_ROSE = Registry.register(
+            Registries.ITEM,
+            Identifier.of(RosesTrades.MOD_ID, "special_rose"),
+            new SpecialRoseItem(new Item.Settings().maxCount(64))
+    );
 
-        public static final Item ROSE = Registry.register(
-                Registries.ITEM,
-                Identifier.of(RosesTrades.MOD_ID, "rose"),
-                new RoseItem(new Item.Settings().maxCount(64))
-        );
+    public static final Item ROSE = Registry.register(
+            Registries.ITEM,
+            Identifier.of(RosesTrades.MOD_ID, "rose"),
+            new RoseItem(new Item.Settings().maxCount(64))
+    );
 
-        public static final Item BOUQUET = Registry.register(
-                Registries.ITEM,
-                Identifier.of(RosesTrades.MOD_ID, "bouquet"),
-                new Item(new Item.Settings().maxCount(64))
-        );
+    public static final Item BOUQUET = Registry.register(
+            Registries.ITEM,
+            Identifier.of(RosesTrades.MOD_ID, "bouquet"),
+            new Item(new Item.Settings().maxCount(64))
+    );
 
-        public static void registerModItems() {
-                RosesTrades.LOGGER.info("Registering mod items for " + RosesTrades.MOD_ID);
-        }
-        }
+    public static void registerModItems() {
+        RosesTrades.LOGGER.info("Registering mod items for " + RosesTrades.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.NATURAL).register(entries -> {
+            entries.add(SPECIAL_ROSE);
+            entries.add(ROSE);
+            entries.add(BOUQUET);
+        });
+    }
+}
